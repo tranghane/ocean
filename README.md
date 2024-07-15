1db899447c244a34d031332abc01feff17211971
Your Name <trungduongnguyen2004@gmail.com>
2024-07-14 22:12:06 -0700

    first look

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to Ocean
+this is a landing page and social media links for Ocean: https://tranghane.github.io/ocean/ to be fix
+# How to run project
+- `npm install`
+- `npm run dev`
+# Sponsors
+- Font Awesome 
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import './index.css'
+import RawCard from './Raw.jsx'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
