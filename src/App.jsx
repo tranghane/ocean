6c3ce603966e5ecb771aaea0e35c3e92b34b41de
Your Name <trungduongnguyen2004@gmail.com>
2024-07-15 23:04:04 -0700

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,9 @@ import { MeshLineGeometry, MeshLineMaterial } from "meshline";
 // import { useControls } from 'leva'
 
 extend({ MeshLineGeometry, MeshLineMaterial });
-// useGLTF.preload(
-//   "tag.glb"
-// );
 useGLTF.preload(
   "final.glb"
+  // open this in blender: https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/5huRVDzcoDwnbgrKUo1Lzs/53b6dd7d6b4ffcdbd338fa60265949e1/tag.glb
 );
 useTexture.preload(
   "band.jpg"
